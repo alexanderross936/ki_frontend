4f5d7f93a414558624271f2631dcdb587a847328
Alexander Ross <alexanderross936@gmail.com>
2021-03-16 14:15:46 -0500

    package.json

---
+++ build/precache-manifest.0d1fb396cfa49c47fabc0a6871607457.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e69a18cf05c47e4899f07755277820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc35e10a6c642e20b056",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "35d2e0c609cf0ab83143",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc35e10a6c642e20b056",
+    "url": "/static/js/2.fa96cf7c.chunk.js"
+  },
+  {
+    "revision": "545961603d9c26a8f030f5d433ac3cae",
+    "url": "/static/js/2.fa96cf7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d2e0c609cf0ab83143",
+    "url": "/static/js/main.238dcb09.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
