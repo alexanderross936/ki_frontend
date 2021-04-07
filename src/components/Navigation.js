1bd0bf9a72b65f01beab63402cc49157b95b22c1
Alexander Ross <alexanderross936@gmail.com>
2021-04-07 13:22:50 -0500

    deploy

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -15,9 +15,17 @@ const Navigation = ({ auth: { isAuthenticated, loading }, logout }) => {
 
         
     const authLinks = (<Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav mr-auto">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/Home">Home <span class="sr-only">(current)</span></a>
+      </li>
       <li className="nav-item">
         <a className="nav-link" href="/sum">Ingredients List</a>
       </li>
